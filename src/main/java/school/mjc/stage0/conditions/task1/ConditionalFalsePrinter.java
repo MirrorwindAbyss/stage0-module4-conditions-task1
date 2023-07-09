9c35cf3e4d519f4af0d3e9ff04db0d92bcc19259
Nabiyev Abdumalik <abdumalikunusov06@gmail.com>
2023-07-09 12:29:12 +0500

    Done!

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        int x, y;
+        x=40;
+        y=50;
+        if(x!=y){
+            System.out.println("False");
+        }
+        else {
+            System.out.println("True");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int x, y;
+        x=5;
+        y=5;
+        if(x==y){
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
     }
 }
